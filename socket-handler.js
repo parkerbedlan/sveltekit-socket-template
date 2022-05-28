a6911492afa7677ee49095cf80e8456434551401
Parker Bedlan <parkerbedlan@gmail.com>
2022-05-28 02:19:33 -0500

    added debugging statements

--- socket-handler.js
+++ socket-handler.js
@@ -8,15 +8,19 @@ export default function injectSocketIO(server) {
 	io.on('connection', (socket) => {
 		// Generate a random username and send it to the client to display it
 		let username = `User ${Math.round(Math.random() * 999999)}`;
+		console.log('connection made with user', username);
 		socket.emit('name', username);
+		console.log('emitted username', username);
 
 		// Receive incoming messages and broadcast them
 		socket.on('message', (message) => {
+			console.log('received message', message);
 			io.emit('message', {
 				from: username,
 				message: message,
 				time: new Date().toLocaleString()
 			});
+			console.log('emitted message', message);
 		});
 	});
 
